26a49c202187c1f006ab3cae9273a1a5770511d3
ronaldo fraga da silva <fotografofraga@gmail.com>
2024-11-17 10:27:36 -0300

    Update conversor_de_porcentagem_terminal.py
    terminei o conversor para terminal

--- conversor_de_porcentagem_terminal.py
+++ conversor_de_porcentagem_terminal.py
@@ -1,1 +1,8 @@
+print("Conversor de porcentagem em páginas lidas")
 
+total_de_paginas = int(input('Coloque o total de páginas:  '))
+porcentagem = int(input('Coloque a porcentagem lida:  '))
+
+paginas_lidas = (total_de_paginas / 100)  * porcentagem
+
+print('Ate agora eu li {:.0f} paginas'.format(paginas_lidas))
